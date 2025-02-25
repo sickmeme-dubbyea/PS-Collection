2b21d5a5f28efbc2e0ffca2ee2eed183c56526a2
Charles Witherspoon <65557557+sickmeme-dubbyea@users.noreply.github.com>
2025-02-25 17:18:06 -0500

    Create adClone.ps1

---
+++ AD/adClone.ps1
@@ -0,0 +1,35 @@
+# Quick script to clone an user user. 
+
+$jsonObject = @(
+    @{
+        "OLDSAM" = ""
+        "NEWSAM" = ""
+        "Type" = ""
+    }
+)
+ 
+# Replace with your Administrative OU
+$AdminOU = "OU=Administrators,OU=$DIVISION,DC=com"
+ 
+foreach ($item in $jsonObject) {
+    $prev = Get-ADUser $item.OldSam -Properties Manager, Division, MemberOf
+ 
+    $newUserParams = @{
+        SamAccountName = $item.NEWSAM
+        Enabled = $false
+        Manager = $prev.Manager
+        DisplayName = "$($item.NEWSAM) (SA)"
+        Division = $prev.Division
+        UserPrincipalName = "$($item.NEWSAM)@assaabloy.com"
+        Path = $AdminOU
+        Name = $item.NEWSAM
+    }
+ 
+    New-ADUser @newUserParams
+    Set-ADUser $item.NEWSAM -Replace @{EmployeeType = $item.Type}
+ 
+    # Add user to groups
+    Add-ADPrincipalGroupMembership -Identity $item.NEWSAM -MemberOf $prev.MemberOf
+}
+
+has context menu
